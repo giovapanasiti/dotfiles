151f953674e76762a825481761073a664c883584
Lorenzo Pagano <lorenzo.pagano@gmail.com>
2016-05-04 01:41:30 +0200

    updates

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,4 +1,3 @@
-#alias restart.finder=""
-#alias show.files=""
-#alias hide.files=""
-alias c='clear'
+alias restart.finder="killall Finder /System/Library/CoreServices/Finder.app"
+alias show.files="defaults write com.apple.finder AppleShowAllFiles YES;restart.finder"
+alias hide.files="defaults write com.apple.finder AppleShowAllFiles NO;restart.finder"
