6670fa4533f72ad1f8c8b13f099aea3efee6e104
Lorenzo Pagano <lorenzo.pagano@gmail.com>
2016-05-08 21:35:58 +0200

    More aliases

--- osx/aliases.zsh
+++ osx/aliases.zsh
@@ -1,3 +1,5 @@
 alias restart.finder="killall Finder /System/Library/CoreServices/Finder.app"
 alias show.files="defaults write com.apple.finder AppleShowAllFiles YES;restart.finder"
 alias hide.files="defaults write com.apple.finder AppleShowAllFiles NO;restart.finder"
+
+alias ror="cd ~/Development/ruby/rails"
